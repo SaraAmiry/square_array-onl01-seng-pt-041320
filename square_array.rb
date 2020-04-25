ae47df171739e4e89680e800d2d62dbcce9bdce9
Sara Amiry <saraamiry24@gmail.com>
2020-04-25 03:41:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,7 @@
 def square_array(array)
-  array.each do |num|
-    print "this is square of #{num}"
+  squared_numbers = []
+  array.each {|num| squared_numbers << num ** 2}
+  squared_numbers
 end
  numbers = [1, 2, 3, 4]
+ print square_array(nubmers)
